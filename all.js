a5a9015829538c42d2251c3f7fdbc638efdf07f2
PiotrKwiecien <taxman42.pk@gmail.com>
2023-05-10 15:00:24 +0200

    initial commit

---
+++ all.js
@@ -0,0 +1,7 @@
+// window.addEventListener('DOMContentLoaded', () => {
+//     const content = document.querySelector('#offers-wrapper');
+//     const contentInitialPadding = getComputedStyle(content).paddingTop.replace('px', '');
+//     const header = document.querySelector('header');
+//     const footer = document.querySelector('footer');
+//
+// })
